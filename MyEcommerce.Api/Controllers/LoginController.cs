5ad7ab23e5652c1b64341a2a439dea07018ed963
Tiago Aguiar <tiago.aguiar1@hotmail.com>
2021-08-04 10:44:32 -0300

    refactor(login): removes example methods

--- MyEcommerce.Api/Controllers/LoginController.cs
+++ MyEcommerce.Api/Controllers/LoginController.cs
@@ -36,25 +36,5 @@ namespace MyEcommerce.Api.Controllers
                 token = token
             });
         }
-
-        [HttpGet]
-        [Route("anonymous")]
-        [AllowAnonymous]
-        public string Anonymous() => "Anônimo";
-
-        [HttpGet]
-        [Route("authenticated")]
-        [Authorize]
-        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
-
-        [HttpGet]
-        [Route("employee")]
-        [Authorize(Roles = "employee,manager")]
-        public string Employee() => "Funcionário";
-
-        [HttpGet]
-        [Route("manager")]
-        [Authorize(Roles = "manager")]
-        public string Manager() => "Gerente";
     }
 }
